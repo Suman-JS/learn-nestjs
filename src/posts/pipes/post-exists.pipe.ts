15433fb31a4632119ffc6a188b94774c49ed5f2d
Suman Mondal <sm7135120@gmail.com>
2025-06-08 16:31:35 +0530

    feat: added custom pipe to check if user exists or not

---
+++ src/posts/pipes/post-exists.pipe.ts
@@ -0,0 +1,22 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  NotFoundException,
+  PipeTransform,
+} from "@nestjs/common";
+
+import { PostsService } from "@/posts/posts.service";
+
+@Injectable()
+export class PostExistsPipe implements PipeTransform {
+  constructor(private readonly postService: PostsService) {}
+
+  async transform(value: any, metadata: ArgumentMetadata) {
+    try {
+      await this.postService.findOne(value);
+    } catch (error) {
+      throw new NotFoundException(`Post with id: ${value} not found.`);
+    }
+    return value;
+  }
+}
