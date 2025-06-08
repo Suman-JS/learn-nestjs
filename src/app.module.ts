940225beee120ff883f262dedfdabd0f62d52f52
Suman Mondal <sm7135120@gmail.com>
2025-06-08 16:45:24 +0530

    feat: added db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { ConfigWrapperModule } from "@/config/typed-config";
-import { validateEnv } from "@/env";
+import { env, validateEnv } from "@/env";
+import { Post } from "@/posts/entities/post.entity";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PostsModule } from "./posts/posts.module";
@@ -11,6 +13,16 @@ import { PostsModule } from "./posts/posts.module";
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateEnv,
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: env.DB_HOST,
+      port: env.DB_PORT,
+      username: env.DB_USERNAME,
+      password: env.DB_PASSWORD,
+      database: env.DB_NAME,
+      entities: [Post],
+      synchronize: env.NODE_ENV === "development" ? true : false,
     }),
     ConfigWrapperModule,
     PostsModule,
