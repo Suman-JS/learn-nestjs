5bdf0ac25ad274a960d690ad5ede80ca5403bce1
Suman Mondal <sm7135120@gmail.com>
2025-06-08 16:47:14 +0530

    feat: registered typeorm

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { Post } from "@/posts/entities/post.entity";
 import { PostsController } from "./posts.controller";
 import { PostsService } from "./posts.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
   providers: [PostsService],
 })
